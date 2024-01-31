b175ee3f6aefb591093a40567b437077f8d04875
Tal Frankenthal <154238085+talfran22@users.noreply.github.com>
2024-01-31 17:34:15 +0200

    upload

--- SpellChecker.java
+++ SpellChecker.java
@@ -11,25 +11,48 @@ public class SpellChecker {
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
+			return str.substring(1);
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+		if(word1.length() == 0){
+			return word2.length();
+		}
+		if(word2.length() == 0){
+			return word1.length();
+		}
+		if(word1.charAt(0) == word2.charAt(0)){
+			return levenshtein(tail(word1),tail(word2));
+		}
+		int min = (int)(Math.min(levenshtein(tail(word1), word2),  levenshtein(word1, tail(word2))));
+		return 1 + (int)(Math.min(min, levenshtein(tail(word1), tail(word2))));
 	}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
+		int i = 0;
 		In in = new In(fileName);
 
-		// Your code here
-
+		while (!in.isEmpty()) {
+			dictionary[i] = in.readLine();
+			i++;
+		}
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
+		int min = word.length();
+		String mini = word;
+		for(int i = 0 ; i < dictionary.length ; i++){
+			int levi = levenshtein(word, dictionary[i]);
+			if(levi <= threshold){
+				if(levi < min){
+					min = levi;
+					mini = dictionary[i];
+				}
+			}
+		}
+		return mini;
 	}
 
 }
